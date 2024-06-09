efb81d4fa5c38c35e3b70452061f0858109ad5e4
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-06-09 14:00:26 +0900

    Created base project, removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Churkin Vitaly on 09.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
